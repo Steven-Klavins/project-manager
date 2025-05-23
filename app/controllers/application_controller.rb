4d437008286f0c49192dc348fef4b5f87b954589
Steven-Klavins <stevenklavins94@gmail.com>
2025-05-23 07:57:47 +0100

    add admin type to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  # Check for admin privileges.
+  def authenticate_admin!
+    redirect_to root_path unless current_user.admin?
+  end
 end
